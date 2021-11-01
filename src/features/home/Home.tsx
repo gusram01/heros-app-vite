37bf14e19361d41e6654fd97f2cdb63213e53ecd
Gus Ramírez <gustavo@coconutt.io>
2021-10-31 22:13:11 -0600

    feat: init with search logic

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,9 +1,38 @@
-import { Container } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  FormControl,
+  FormLabel,
+  Input,
+} from '@chakra-ui/react';
+import { SyntheticEvent } from 'react';
 
 const Home = () => {
+  const searchHeros = (event: SyntheticEvent) => {
+    event.preventDefault();
+  };
+
   return (
     <Container minH="calc(100vh - 96px)" minW="100%">
-      <h1>Home</h1>
+      <Center>
+        <form onSubmit={searchHeros}>
+          <Box p={6} maxW={96}>
+            <FormControl>
+              <FormLabel>Search</FormLabel>
+
+              <Input type="text" placeholder="type your hero..." />
+            </FormControl>
+
+            <Center mt={6}>
+              <Button type="submit" colorScheme="yellow" variant="outline">
+                search
+              </Button>
+            </Center>
+          </Box>
+        </form>
+      </Center>
     </Container>
   );
 };
